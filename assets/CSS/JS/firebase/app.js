9931facb7055ec425c757b691528566e5e0a2d1c
Ana Caroline Oliveira Lopes dos Reis <karolinne.oreis@gmail.com>
2022-09-06 16:01:20 -0300

    mundo invertido

---
+++ assets/CSS/JS/firebase/app.js
@@ -0,0 +1,19 @@
+
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.9.4/firebase-app.js"
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyCYLnXPW9oRwGrlxLw0QO7ZYVvCYAHoyjM",
+    authDomain: "projeto-teste-a2622.firebaseapp.com",
+    projectId: "projeto-teste-a2622",
+    storageBucket: "projeto-teste-a2622.appspot.com",
+    messagingSenderId: "812481274935",
+    appId: "1:812481274935:web:03e4a3a6902243c109cbb2"
+  }
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig)
+ export default app
